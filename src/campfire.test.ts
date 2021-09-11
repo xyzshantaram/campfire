b5dc52337caede42e33c98a69e2f89c2c440328c
Siddharth Singh <simsid66@gmail.com>
2021-09-11 13:27:00 +0530

    add tests for mustache

--- src/campfire.test.ts
+++ src/campfire.test.ts
@@ -1,6 +1,6 @@
 const { describe, test, expect } = require("@jest/globals");
 // @ts-ignore
-const { escape, unescape } = require('../dist/campfire.cjs');
+const { escape, unescape, mustache, template } = require('../dist/campfire.cjs');
 
 describe('tests for escape() and unescape()', () => {
     let escaped = '&amp;&lt;&gt;&quot;&#39;/';
@@ -21,26 +21,43 @@ describe('tests for escape() and unescape()', () => {
         expect(escape(unescape(escaped))).toStrictEqual(escaped);
     });
 
-    test('should unescape entities in order', function () {
+    test('should unescape entities in order', () => {
         expect(unescape("&amp;lt;")).toStrictEqual("&lt;");
     });
 
-    test('should unescape the proper entities', function () {
+    test('should unescape the proper entities', () => {
         expect(unescape(escaped)).toStrictEqual(unescaped);
     });
 
-    test('should handle strings with nothing to unescape', function () {
+    test('should handle strings with nothing to unescape', () => {
         expect(unescape('abc')).toStrictEqual('abc');
     });
 
-    test('should unescape the same characters escaped by `escape`', function () {
+    test('should unescape the same characters escaped by `escape`', () => {
         expect(unescape(escape(unescaped))).toBe(unescaped);
     });
 
-    test('should handle leading zeros in html entities', function () {
+    test('should handle leading zeros in html entities', () => {
         expect(unescape('&#39;')).toStrictEqual("'");
         expect(unescape('&#039;')).toStrictEqual("'");
         expect(unescape('&#000039;')).toStrictEqual("'");
     });
 
+})
+
+describe('Tests for mustache', () => {
+    test('should substitute data that is present', () => {
+        expect(mustache("Welcome to {{ location }}, {{ name }}.", { name: "John", location: "Mars" })).toBe("Welcome to Mars, John.");
+    })
+    test('should ignore spaces surrounding mustache names', () => {
+        expect(mustache("{{ name }} {{name}} {{  name  }} {{    name    }}", { name: "John" })).toBe("John John John John");
+    })
+
+    test('Should ignore escaped mustaches', () => {
+        expect(mustache("\\{{ name }}", { name: "John" })).toBe("{{ name }}");
+    })
+
+    test('Should ignore absent mustaches', () => {
+        expect(mustache("{{ nonexistent }}", { name: "John" })).toBe("{{ nonexistent }}");
+    })
 })
