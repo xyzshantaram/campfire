7980ab7d35a2cffbe824f540b223f9ed9f709e1c
Siddharth Singh <simsid66@gmail.com>
2021-09-13 08:23:19 +0530

    create examples/button

---
+++ examples/button/main.js
@@ -0,0 +1,29 @@
+// A button that keeps track of how many times it's been clicked
+let count = 0;
+const btn = cf.nu("button#id.class1.class2", {
+  innerHTML: "I have not been clicked.",
+  attrs: {
+    // DOM attributes
+    "data-an-attribute": 42,
+  },
+  on: {
+    // event handlers, assigned using addEventListener
+    "click": function (e) {
+      this.innerHTML = `I have been clicked ${++count} times.`;
+    },
+  },
+  style: {
+    // Uses property names as specified in CSSStyleDeclaration.
+    background: "#007cdf",
+    borderRadius: "0.25em",
+    margin: "0.5rem",
+    color: "#f5f4f0",
+    transitionDuration: "0.2s",
+    border: "2px solid black",
+  }, // styles
+  misc: {
+    // miscellaneous properties
+    type: "button",
+  },
+});
+document.body.appendChild(btn);
