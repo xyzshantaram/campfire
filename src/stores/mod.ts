1b6826b5237de02fb3d816fba43ee8bd5f1b441d
Siddharth Singh <me@shantaram.xyz>
2025-04-07 01:50:10 +0530

    properly overload store factory

--- src/stores/mod.ts
+++ src/stores/mod.ts
@@ -2,22 +2,19 @@ import { ListStore } from "./ListStore.ts";
 import { MapStore } from "./MapStore.ts";
 import { Store } from "./Store.ts";
 
-export type StoreInitializer<T> = {
-    type: 'list',
-    value?: Array<T>,
-} | {
-    type: 'map',
-    value?: Record<string, T>
-} | {
-    value?: T
-}
+export type StoreInitializer<T> =
+    | { type: 'list'; value?: Array<T> }
+    | { type: 'map'; value?: Record<string, T> }
+    | { value?: T };
 
-export const store = <T>(opts: StoreInitializer<T>) => {
+export function store<T>(opts: { type: 'list'; value?: T[] }): ListStore<T>;
+export function store<T>(opts: { type: 'map'; value?: Record<string, T> }): MapStore<T>;
+export function store<T>(opts: { value?: T }): Store<T>;
+export function store<T>(opts: StoreInitializer<T>): Store<any> {
     if ('type' in opts) {
         if (opts.type === 'list') return new ListStore(opts.value);
-        else if (opts.type === 'map') return new MapStore(opts.value);
+        if (opts.type === 'map') return new MapStore(opts.value);
     }
     return new Store<T>(opts.value);
 }
-
-export { MapStore, ListStore, Store };
+export { Store, ListStore, MapStore };
