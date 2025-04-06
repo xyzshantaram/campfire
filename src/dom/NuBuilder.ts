166eab17e854474f172b2ee0c0dba4816da7de9d
Siddharth Singh <me@shantaram.xyz>
2025-04-06 23:48:10 +0530

    add docs for builder API

--- src/dom/NuBuilder.ts
+++ src/dom/NuBuilder.ts
@@ -3,11 +3,19 @@ import { ElementProperties, InferElementType, RenderFunction, StringStyleProps, 
 import { extend } from "./nu";
 
 
+/**
+ * Creates a typed HTML element based on the tag name.
+ * 
+ * @param name - The HTML tag name.
+ * @returns A newly created HTML element of the specified type.
+ * @internal
+ */
 const createTypedElement = <K extends keyof HTMLElementTagNameMap>(name: K) => {
     return document.createElement(name);
 }
 
 /**
+ * Parses a tag string into its component parts.
  * 
  * @param str A string to parse, of the form tag#id.classes[.classes].
  * @returns A `TagStringParseResult` object containing the parsed information.
@@ -44,14 +52,29 @@ const parseEltString = (str: string | undefined): TagStringParseResult => {
  * ```
  */
 export class NuBuilder<T extends string, E extends InferElementType<T>, D extends Record<string, Store<any>>> {
+    /** Element properties configuration object */
     props: ElementProperties<E, D> = {};
+
+    /** Element info string (tag, id, classes) */
     info: T;
 
+    /**
+     * Creates a new NuBuilder instance.
+     * 
+     * @param info - A string describing the element in the format 'tag#id.class1.class2'
+     * @param props - Optional initial properties for the element
+     */
     constructor(info: T, props?: ElementProperties<E, D>) {
         if (props) this.props = props;
         this.info = info;
     }
 
+    /**
+     * Finalizes the builder and creates the actual DOM element with all configured properties.
+     * 
+     * @returns A tuple containing the created element as the first item, followed by any child elements
+     * @throws Error if a class name contains a '#' character
+     */
     done(): [E, ...HTMLElement[]] {
         let { tag, id, classes = [] } = parseEltString(this.info);
 
@@ -71,27 +94,59 @@ export class NuBuilder<T extends string, E extends InferElementType<T>, D extend
         return extend(elem as E, this.props);
     }
 
+    /**
+     * Sets the content of the element.
+     * 
+     * @param value - Either a string of content or a render function that returns content
+     * @returns The builder instance for chaining
+     */
     content(value: string | RenderFunction<E, D>) {
         this.props.contents = value;
         return this;
     }
 
+    /**
+     * Sets a single attribute on the element.
+     * 
+     * @param name - The attribute name
+     * @param value - The attribute value
+     * @returns The builder instance for chaining
+     */
     attr(name: string, value: string | boolean | number) {
         this.props.attrs ||= {};
         this.props.attrs[name] = value.toString();
         return this;
     }
 
+    /**
+     * Sets multiple attributes on the element at once.
+     * 
+     * @param value - An object containing attribute name-value pairs
+     * @returns The builder instance for chaining
+     */
     attrs(value: Required<ElementProperties<E, D>['attrs']>) {
         this.props.attrs = value;
         return this;
     }
 
+    /**
+     * Sets whether the content value should be treated as raw HTML.
+     * 
+     * @param value - If true, content will not be escaped before setting innerHTML
+     * @returns The builder instance for chaining
+     */
     raw(value: boolean) {
         this.props.raw = value;
         return this;
     }
 
+    /**
+     * Sets miscellaneous properties on the element (e.g. `input.checked`).
+     * 
+     * @param obj - Either a property name or an object containing multiple properties
+     * @param value - The value for the property if obj is a property name
+     * @returns The builder instance for chaining
+     */
     misc(obj: string, value: unknown): NuBuilder<T, E, D>;
     misc(obj: Record<string, unknown>): NuBuilder<T, E, D>;
     misc(obj: string | Record<string, unknown>, value?: unknown): NuBuilder<T, E, D> {
@@ -101,23 +156,49 @@ export class NuBuilder<T extends string, E extends InferElementType<T>, D extend
         return this;
     }
 
+    /**
+     * Sets a single CSS style property on the element.
+     * 
+     * @param prop - The CSS property name
+     * @param value - The CSS property value
+     * @returns The builder instance for chaining
+     */
     style(prop: StringStyleProps, value: string) {
         this.props.style ||= {};
         this.props.style[prop] = value;
         return this;
     }
 
+    /**
+     * Sets multiple CSS style properties on the element at once.
+     * 
+     * @param value - An object containing style name-value pairs
+     * @returns The builder instance for chaining
+     */
     styles(value: Required<ElementProperties<E, D>['style']>) {
         this.props.style = value;
         return this;
     }
 
+    /**
+     * Attaches an event handler to the element.
+     * 
+     * @param type - The event type to listen for (e.g., 'click', 'submit')
+     * @param handler - The event handler function
+     * @returns The builder instance for chaining
+     */
     on<K extends keyof HTMLElementEventMap>(type: K, handler: (event: HTMLElementEventMap[K]) => void) {
         this.props.on ||= {};
         this.props.on[type] = handler as DOMEventHandlers[K];
         return this;
     }
 
+    /**
+     * Specifies selectors for elements that should be retrieved after building the element.
+     * 
+     * @param selectors - A single selector string or an array of selector strings
+     * @returns The builder instance for chaining
+     */
     gimme(selectors: string | string[]) {
         this.props.gimme ||= [];
         if (Array.isArray(selectors)) this.props.gimme = selectors;
