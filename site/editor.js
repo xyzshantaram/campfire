2ded7875d7489fe24e68423348d944eac448b7fd
Siddharth Singh <me@shantaram.xyz>
2025-04-09 02:53:27 +0530

    add basic styling to editor.js

--- site/editor.js
+++ site/editor.js
@@ -1,28 +1,57 @@
 import * as cf from 'https://esm.sh/campfire.js@4.0.0-rc4';
 import { highlight } from 'https://esm.sh/macrolight@1.5.0';
-import toml from 'https://esm.sh/toml';
-
+import toml from 'https://esm.sh/toml@3.0.0';
 import { CodeJar } from 'https://esm.sh/codejar@4.2.0';
 
-const iframeContentTemplate = cf.template(`\
+const iframeContentTemplate = cf.template(cf.html`\
 <html>
 <head>
     <title>Campfire Playground</title>
     <style>
+        body {
+            color: white;
+            line-height: 1.6;
+        }
+
+        input {
+            border-top: none;
+            border-left: none;
+            border-right: none;
+            border-bottom: 2px solid white;
+        }
+
+        button, input {
+            padding: 0.2rem;
+            font-size: inherit;
+            color: inherit;
+            background-color: transparent;
+            min-width: 10ch;
+            margin-top: 0.4rem;
+            margin-bottom: 0.4rem;
+        }
+
+        button {
+            margin-left: 0.4rem;
+            border-radius: 0.2rem;
+            border: 2px solid #ff9a00;
+        }
+
+        input:focus {
+            border-bottom-color: #ff9a00;
+        }
         {{ css }}
     </style>
 </head>
 <body>
     {{ html }}
     <script type='module'>
-        import cf from 'https://esm.sh/campfire.js@4.0.0-rc4';
+        import cf from 'https://esm.sh/campfire.js@4.0.0-rc7';
         window.onload = function() {
             {{ js }}
         }
     </script>
 </body>
-</html>\
-`, false);
+</html>`, false);
 
 const JS_KEYWORDS = ['break', 'case', 'catch', 'class', 'const', 'continue', 'debugger', 'default', 'delete', 'do', 'else', 'export', 'extends', 'finally', 'for', 'function', 'if', 'import', 'in', 'instanceof', 'let', 'new', 'return', 'super', 'switch', 'this', 'throw', 'try', 'typeof', 'var', 'void', 'while', 'with', 'yield'];
 const HTML_KEYWORDS = ['<!DOCTYPE', 'html', 'head', 'body', 'title', 'script', 'style', 'meta', 'link'];
