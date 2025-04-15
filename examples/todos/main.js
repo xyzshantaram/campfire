e613c0148fdb6520d60d1c72bffde7c2cea5d20d
Siddharth Singh <me@shantaram.xyz>
2025-04-15 16:14:16 +0530

    update todo example

--- examples/todos/main.js
+++ examples/todos/main.js
@@ -1,26 +1,19 @@
-const store = new cf.store({ type: "list", value: [] });
+const store = cf.store({ type: "list", value: [] });
+const storeId = cf.ids('todo');
 
 const [input] = cf.nu('input')
     .style('min-width', '20ch')
     .misc('type', 'text')
     .done();
 
-const button = cf.nu("button")
+const [button] = cf.nu("button")
     .misc('type', 'button')
     .on('click', () => {
         const value = input.value.trim();
-        if (value) store.push({ done: false, name: value });
+        if (value) store.push({ done: false, name: value, id: storeId() });
         input.value = '';
     })
     .content("Add")
-    .done();
-
-const form = cf.nu('div')
-    .html(cf.html`
-        <cf-slot name="input"></cf-slot>
-        <cf-slot name="button"></cf-slot>
-    `)
-    .children({ input, button })
     .done();
 
 const TodoItem = (store, event) => cf.nu('li.todo-item')
@@ -28,16 +21,14 @@ const TodoItem = (store, event) => cf.nu('li.todo-item')
         <span>${event.value.name}</span>
         <cf-slot name='remove'></cf-slot>
     `)
-    .attr('data-idx', event.idx)
+    .track(event.value.id)
     .children({
         remove: cf.nu('button')
             .content("Mark done")
-            .on('click',
-                function () {
-                    const idx = +this.parentElement.getAttribute('data-idx');
-                    store.set(idx, { done: true, name: event.value.name })
-                }
-            )
+            .on('click', () => {
+                const idx = store.findIndex(itm => itm.id === event.value.id);
+                store.set(idx, { ...event.value, done: true });
+            })
             .ref()
     })
     .done()
@@ -46,29 +37,26 @@ const todos = cf.nu('ul')
     .deps({ items: store })
     .render((_, { event, elt }) => {
         if (!event) return;
-        // fine-grained update
         switch (event.type) {
             case "append": {
                 cf.insert(TodoItem(store, event), { into: elt });
                 break;
             }
             case "change": {
-                const [li] = cf.select({ s: `li[data-idx="${event.idx}"]`, from: elt });
-                li.classList.add('deleted');
+                const li = cf.tracked(event.value.id);
+                li.classList.add('done');
                 const [btn] = cf.select({ s: 'button', from: li });
-                cf.extend(btn, {
-                    contents: "Delete",
-                    on: { click: () => store.remove(Number(li.getAttribute('data-idx'))) }
-                });
+                cf.nu(btn)
+                    .content("Delete")
+                    .on('click', () => store.remove(store.findIndex(itm => itm.id === event.value.id)))
+                    .done();
                 break;
             }
             case "deletion": {
-                const [li] = cf.select({ s: `li.todo-item[data-idx="${event.idx}"]`, from: elt });
-                if (li) li.remove();
-
-                cf.select({ s: 'li.todo-item', from: elt, all: true }).forEach((elem, idx) => {
-                    elem.setAttribute('data-idx', idx);
-                });
+                const li = cf.tracked(event.value.id);
+                if (!li) break;
+                li.remove();
+                cf.untrack(event.value.id);
                 break;
             }
             default:
@@ -87,7 +75,7 @@ const app = cf.nu("#app")
         <cf-slot name='todos'></cf-slot>
     `)
     .style('fontFamily', 'sans-serif')
-    .children({ form, todos })
+    .children({ form: [input, button], todos })
     .done();
 
 cf.insert(app, { into: document.body });
